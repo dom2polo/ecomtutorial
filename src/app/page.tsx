d51b2cce7a1aa5c1e35dacfb335f082c21abe666
dom2polo <125336032+dom2polo@users.noreply.github.com>
2024-10-16 12:00:44 -0500

    add button in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Button, buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
-  return <MaxWidthWrapper>
+  return (
+    <>
+  <MaxWidthWrapper>
     <div className="py-20 max-auto text-center flex flex-col items-center max-w-3xl">
       <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
         Your Market Place for High-Quality{' '}
@@ -15,8 +18,23 @@ export default function Home() {
           platform is verified by our team to make 
           sure you have the best shopping experience
         </p>
-        <div className="flex flex-col sm:flex-row gap-4 mt-6"></div>
-        <Link href="/products"> Browse Trending </Link>
+        <div className="flex flex-col sm:flex-row gap-4 mt-6">
+        <Link href="/products" className={buttonVariants()}> Browse Trending </Link>
+        <Button variant='ghost'>Our Quality Promise &rarr; </Button>
+        </div>
     </div>
+
+    {/* TODO list: products */}
   </MaxWidthWrapper>
+
+  <section className="border-t border-gray-200 bg-gray-50">
+    <MaxWidthWrapper className="py-20">
+      <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-0 ">
+        
+      </div>
+    </MaxWidthWrapper>
+  </section>
+
+  </> 
+  )
 }
