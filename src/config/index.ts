001666f91caa99180a02378f97a528d0f0cb1f8c
dom2polo <125336032+dom2polo@users.noreply.github.com>
2024-10-16 13:53:17 -0500

    setting up navbar

---
+++ src/config/index.ts
@@ -0,0 +1,45 @@
+export const PRODUCT_CATEGORIES = [
+    {
+        label: "UI Kits",
+        value: "ui_kits" as const,
+        feature: [
+            {
+                name: "Editor picks",
+                href: "#",
+                imageSrc: '/nav/ui-kits/mixed.jpg',
+            },
+            {
+                name: "New Arrivals",
+                href: "#",
+                imageSrc: "/nav/ui-kits/blue.jpg",
+            },
+            {
+                name: "Best Sellers",
+                href: "#",
+                imageSrc: "/nav/ui-kits/purple.jpg",
+            },
+        ],
+    },
+    {
+        label: "Icons",
+        value: "icons" as const,
+        feature: [
+            {
+                name: "Favorite Icon Picks",
+                href: "#",
+                imageSrc: "/nav/icons/pick.jpg",
+            },
+            {
+                name: "New Arrivals",
+                href: "#",
+                imageSrc: "/nav/icons/new.jpg",
+            },
+            {
+                name: "Bestselling Icons",
+                href: "#",
+                imageSrc: "/nav/ui-kits/bestsellers.jpg",
+            },
+        ],
+    },
+    
+]
